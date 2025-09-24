c73cd89d21a3358ba5c70e8070913c37f8b73318
dksrinath <152683667+dksrinath@users.noreply.github.com>
2025-09-25 00:29:02 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://project-management-tool-d41q.onrender.com/';
+const API_URL = 'https://project-management-tool-d41q.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
